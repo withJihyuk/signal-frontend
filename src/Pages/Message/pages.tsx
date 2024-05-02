30dc3f3bd411078cf1d90d7f8d02a5677985186b
Jihyuk Lee <lead@sver.dev>
2024-05-02 20:51:27 +0900

    :lipstick: :: 채팅 페이지 퍼블리싱

--- src/Pages/Message/pages.tsx
+++ src/Pages/Message/pages.tsx
@@ -1,5 +1,47 @@
 import * as S from "./style.ts";
+import plus from "../../assets/plus.svg";
 
 export default function Message() {
-  return <div>임시</div>;
+  return (
+    <div>
+      <S.ButtonObject>
+        <S.Morebutton>
+          <img
+            src={plus}
+            style={{ marginRight: "7px" }}
+            width="14"
+            height="14"
+            alt="menu"
+          />
+          <S.MoreText>더보기</S.MoreText>
+        </S.Morebutton>
+      </S.ButtonObject>
+      <S.chatObject>
+        <S.chatBox>
+          <S.chatText>디자인 안할래요 ㅋㅋ 이게맞냐</S.chatText>
+        </S.chatBox>
+      </S.chatObject>
+      <S.chatObject>
+        <S.OwnchatBox>
+          <S.chatText style={{ color: "#000000", textAlign: "left" }}>
+            반갑습니다.
+          </S.chatText>
+        </S.OwnchatBox>
+      </S.chatObject>
+
+      <S.chatObject>
+        <S.OwnchatBox>
+          <S.chatText style={{ color: "#000000", textAlign: "left" }}>
+            긴문장을테스트중이에요반갑습니다이런게줄바꿈이된다고하면정말이쁘지않을까요?
+          </S.chatText>
+        </S.OwnchatBox>
+      </S.chatObject>
+      <S.SendMessageObject>
+        <S.AddButton>
+          <img src={plus} width="14" height="14" alt="menu" />
+        </S.AddButton>
+        <S.SendMessageBox />
+      </S.SendMessageObject>
+    </div>
+  );
 }
--- src/Pages/Message/style.ts
+++ src/Pages/Message/style.ts
@@ -1,1 +1,109 @@
 import styled from "styled-components";
+
+export const ButtonObject = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 14px;
+  margin-bottom: 30px;
+`;
+
+export const Morebutton = styled.div`
+  width: 130px;
+  height: 40px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export const MoreText = styled.p`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 14px; /* 93.333% */
+  letter-spacing: -0.5px;
+`;
+
+export const chatText = styled.a`
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 14px; /* 87.5% */
+  letter-spacing: -0.5px;
+  word-break: break-all;
+  text-align: right;
+`;
+
+export const chatBox = styled.div`
+  display: inline-flex;
+  padding: 15px 29px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 100px;
+  background: #f82e62;
+  margin-right: 30px;
+  margin-left: 30px;
+  float: right;
+`;
+
+export const chatObject = styled.div`
+  width: 100%;
+  height: 70px;
+`;
+
+export const OwnchatBox = styled.div`
+  display: inline-flex;
+  padding: 15px 29px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  background: #fff;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+  margin-left: 30px;
+  margin-right: 30px;
+  float: left;
+`;
+
+export const SendMessageObject = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 30px;
+  width: 100%;
+  left: 50%;
+  transform: translate(-50%, 0);
+`;
+
+export const SendMessageBox = styled.div`
+  width: 70%;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 100px;
+  background: #fff;
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.5);
+`;
+
+export const AddButton = styled.div`
+  width: 40px;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 40px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 15px;
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.5);
+`;
